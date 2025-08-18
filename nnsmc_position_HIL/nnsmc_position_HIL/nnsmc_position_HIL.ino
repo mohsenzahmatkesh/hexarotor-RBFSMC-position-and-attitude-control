2aeb84456721c48c4eb15cc1cc2acbb7637e9e30
mohsenzahmatkesh <mhn.zahmat@gmail.com>
2025-08-18 08:46:59 +0100

    nn parameters sending to Simulink

---
+++ nnsmc_position_HIL/nnsmc_position_HIL/nnsmc_position_HIL.ino
@@ -0,0 +1,67 @@
+double g = 9.81;
+double mass = 1; 
+double L = 0.2; 
+double Ix = 0.008; 
+double Iy = 0.008;
+double Iz = 0.017;
+double c = 0.088;
+float W[14][10];
+
+void nn_weight(float W[14][10]){
+  for (int i = 0; i < 14; i++) {
+    for (int j = 0; j < 10; j++){
+      W[i][j] = -1.0f + 2.0f * (float)rand() / (float)RAND_MAX;
+    }
+  }
+}
+void send_to_simulink() {
+  uint8_t buf[3 + 14*10*4];
+  buf[0] = 0xAA;
+  buf[1] = 0xBB;
+  buf[2] = 0xCC;
+
+  int k=3;
+  for(int i = 0; i < 14; i++){
+    for(int j = 0; j < 10; j++){
+      memcpy(&buf[k], &W[i][j], 4);
+      k += 4;
+    }
+  }
+  Serial.write(buf, sizeof(buf));
+
+}
+
+
+
+void setup() {
+  Serial.begin(115200);
+  
+  
+}
+
+void loop() {
+  nn_weight(W);
+  send_to_simulink();
+}
+
+// void readFromMatlab() {
+//   // Read bytes directly into the struct. This is efficient because the
+//   // struct is "packed" and matches the data order from Simulink's Mux block.
+//   byte* buffer = (byte*)&incomingData;
+//   Serial.readBytes(buffer, STATE_PACKET_SIZE);
+// }
+
+// /**
+//  * @brief Sends the entire 40-byte ControlData struct over the serial port.
+//  * @param data The ControlData struct to be sent.
+//  */
+// void writeToMatlab(const StateAndParamsData& data) {
+//   const byte* buffer = (const byte*)&data;
+//   Serial.write(buffer, sizeof(StateAndParamsData));
+//   Serial.write(13);
+//   Serial.write(10);
+// }
+// }
+
+
+
