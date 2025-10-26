23b5af7d3877b4c530eab0369925b29516be24eb
mohsenzahmatkesh <mhn.zahmat@gmail.com>
2025-10-26 02:43:25 +0000

    HIL updated

--- nnsmc_position_HIL/nnsmc_position_HIL/nnsmc_position_HIL.ino
+++ nnsmc_position_HIL/nnsmc_position_HIL/nnsmc_position_HIL.ino
@@ -1,10 +1,10 @@
-double g = 9.81;
-double mass = 1; 
-double L = 0.2; 
-double Ix = 0.008; 
-double Iy = 0.008;
-double Iz = 0.017;
-double c = 0.088;
+float g = 9.81;
+float mass = 1; 
+float L = 0.2; 
+float Ix = 0.008; 
+float Iy = 0.008;
+float Iz = 0.017;
+float c = 0.088;
 
 // Global states received from Simulink
 float x, y, z;
@@ -18,46 +18,47 @@ float u1_prev, u2_prev, u3_prev, u4_prev;
 float W[10][4];   //10x4 matrix
 
 
-double phiddes = 0, phidddes = 0;
-double thetaddes = 0, thetadddes = 0;
-double psiddes = 0, psidddes = 0;
-double xddes = 0, xdddes = 0;
-double yddes = 0, ydddes = 0;
-double zddes = 0, zdddes = 0;
+float phiddes = 0, phidddes = 0;
+float thetaddes = 0, thetadddes = 0;
+float psiddes = 0, psidddes = 0;
+float xddes = 0, xdddes = 0;
+float yddes = 0, ydddes = 0;
+float zddes = 0, zdddes = 0;
 
 
 // SMC parameters
-double cz = 1;
-double eps1 = 0.8;
-double eta1 = 50;
+float cz = 1;
+float eps1 = 0.8;
+float eta1 = 50;
 
-double cpsi = 1.5; 
-double eps2 = 0.8; 
-double eta2 = 50; 
+float cpsi = 1.5; 
+float eps2 = 0.8; 
+float eta2 = 50; 
 
-double ctheta = 1.5;
-double eps3 = 0.5;
-double eta3 = 50; 
-double c3 = 1;
-double c4 = 6;
+float ctheta = 1.5;
+float eps3 = 0.5;
+float eta3 = 50; 
+float c3 = 1;
+float c4 = 6;
 
-double chphi = 1.5;
-double eps4 = 0.5;
-double eta4 = 50; 
-double c7 = 1;
-double c8 = 6;
-
-
-
-
+float chphi = 1.5;
+float eps4 = 0.5;
+float eta4 = 50; 
+float c7 = 1;
+float c8 = 6;
 
 
 // NN parameters
-double Gamma = 100; 
+float Gamma = 100; 
 float RBF_means[14][10];
 float RBF_std[10][1];
 float mu[10];
 float Delta_hat[4];
+float u1, u2, u3, u4;
+float s1, s2, s3, s4;
+float s11, s22, s33, s44;
+
+
 
 void receive_from_simulink() {
 
@@ -134,28 +135,36 @@ void nn_smc(){
     }
   }
 
-  float s1 = cz*(z - zdes) + (zd - zddes);
-  float s11 = tanhf(s1);
-  float u1 = (mass/(cos(phi)*cos(theta)))*(cz*(zd-(zddes)) + zdddes + g + eps1*s11 + eta1*s1 + Delta_hat[3]);
+  s1 = cz*(z - zdes) + (zd - zddes);
+  s11 = tanhf(s1);
+  u1 = (mass/(cos(phi)*cos(theta)))*(cz*(zd-(zddes)) + zdddes + g + eps1*s11 + eta1*s1 + Delta_hat[3]);
 
-  float s2 = cpsi*(psides - psi) + (psiddes - psid);
-  float s22 = tanhf(s2);
-  float u4 = (Iz/c)*(cpsi*(psiddes-psid) + psidddes + eps2*s22 + eta2*s2 + 2*Delta_hat[2]);
+  s2 = cpsi*(psides - psi) + (psiddes - psid);
+  s22 = tanhf(s2);
+  u4 = (Iz/c)*(cpsi*(psiddes-psid) + psidddes + eps2*s22 + eta2*s2 + 2*Delta_hat[2]);
 
 
   float c1 = (11*mass/(cos(phi)*cos(psi)*cos(thetades))) * c3;
   float c2 = (6*mass/(cos(phi)*cos(psi)*cos(thetades))) * c3;
-  float s3 = c1*(xddes - xd) + c2*(xdes - x) + c3*(thetaddes - thetad) + c4*(thetades - theta);
-  float s33 = tanhf(s3);
-  float u3 = (Iy/L)*((c1/c3)*(xdddes-xdd) + (c2/c3)*(xddes-xd) + thetadddes + (c4/c3)*(thetaddes-thetad)  + (1/c3)*(eps3*s33 + eta3*s3) + Delta_hat[1]);
+  s3 = c1*(xddes - xd) + c2*(xdes - x) + c3*(thetaddes - thetad) + c4*(thetades - theta);
+  s33 = tanhf(s3);
+  u3 = (Iy/L)*((c1/c3)*(xdddes-xdd) + (c2/c3)*(xddes-xd) + thetadddes + (c4/c3)*(thetaddes-thetad)  + (1/c3)*(eps3*s33 + eta3*s3) + Delta_hat[1]);
 
 
   float c5 = (-11*mass/((sin(phides)*sin(theta)*sin(psi))+(cos(phides)*cos(psi)))) * c7;
   float c6 = (-6*mass/((sin(phides)*sin(theta)*sin(psi))+(cos(phides)*cos(psi)))) * c7;
-  float s4 = c5*(yddes - yd) + c6*(ydes - y) + c7*(phiddes - phid) + c8*(phides - phi);
-  float s44 = tanhf(s4);
-  float u2 = (Ix/L)*((c5/c7)*(ydddes-ydd) + (c6/c7)*(yddes-yd) + phidddes + (c8/c7)*(phiddes-phid) +  (1/c7)*(eps4*s44 + eta4*s4) + Delta_hat[0]);
+  s4 = c5*(yddes - yd) + c6*(ydes - y) + c7*(phiddes - phid) + c8*(phides - phi);
+  s44 = tanhf(s4);
+  u2 = (Ix/L)*((c5/c7)*(ydddes-ydd) + (c6/c7)*(yddes-yd) + phidddes + (c8/c7)*(phiddes-phid) +  (1/c7)*(eps4*s44 + eta4*s4) + Delta_hat[0]);
 
+  float s[4] = {s4, s3, s2, s1};
+  float W_dot[10][4];
+
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 4; j++) {
+      W_dot[i][j] = Gamma * mu[i] * s[j];
+    }
+  }
 }
 
 
@@ -183,7 +192,11 @@ void send_to_simulink() {
 
   memcpy(&buf[3], &RBF_std[0][0], 40);       // 10 floats
   memcpy(&buf[43], &RBF_means[0][0], 560);   // 140 floats
-  memcpy(&buf[603], &Delta_hat[0], 16);      // 4 floats
+  memcpy(&buf[603], &u1, 4);
+  memcpy(&buf[607], &u2, 4);
+  memcpy(&buf[611], &u3, 4);
+  memcpy(&buf[615], &u4, 4);
+
   // memcpy(&buf[603], &mu[0], 40);
 
   Serial.write(buf, sizeof(buf));
